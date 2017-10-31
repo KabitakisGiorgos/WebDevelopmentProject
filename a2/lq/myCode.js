5e0a7070493265de058c477cf165cc25802b22ad
Giorgos Kampitakis <csd3142@csd.uoc.gr>
2017-11-01 00:10:10 +0200

    Inserted The map verification

--- a2/lq/myCode.js
+++ a2/lq/myCode.js
@@ -1,24 +1,64 @@
 "use strict"
 
-var verify = function(e) {
-    if (document.getElementById('password').value ==
-      document.getElementById('confirm_password').value&& e.keyCode===13) {
+var verify = function(e,flag) {
+    if ((document.getElementById('password').value ==
+      document.getElementById('confirm_password').value&&e.keyCode===13)||(document.getElementById('password').value ==
+      document.getElementById('confirm_password').value&&flag===0)) {
       document.getElementById('message').style.color = 'green';
       document.getElementById('message').innerHTML = 'Correct';
-    } else if(e.keyCode===13){
+    } else if(e.keyCode===13||flag===0){
       document.getElementById('message').style.color = 'red';
       document.getElementById('message').innerHTML = 'Wrong Password';
+      setTimeout(function(){
+        document.getElementById("message").innerHTML="";
+        },3000);
     }
   }
 
-var verify2=function(){
-    if (document.getElementById('password').value ==
-    document.getElementById('confirm_password').value) {
-    document.getElementById('message').style.color = 'green';
-    document.getElementById('message').innerHTML ='Correct';
-  } else {
-    document.getElementById('message').style.color = 'red';
-    document.getElementById('message').innerHTML = 'Wrong Password';
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: {lat: -34.397, lng: 150.644}
+    });
+    var geocoder = new google.maps.Geocoder();
+
+    document.getElementById('city').addEventListener('blur', function() {
+      geocodeAddress(geocoder, map);
+    });
+    document.getElementById('address').addEventListener('click', function() {
+      geocodeAddress(geocoder, map);
+    });
+    document.getElementById('country').addEventListener('click', function() {
+      geocodeAddress(geocoder, map);
+    });
   }
 
-}
+  function geocodeAddress(geocoder, resultsMap) {
+
+    var address = document.getElementById('address').value;
+    var city = document.getElementById('city').value;
+    console.log(document.getElementById('city').value);
+    var country = document.getElementById('country').value;
+    var address2=country+city+address;
+    geocoder.geocode({'address': address2}, function(results, status) {
+      
+      if (status === 'OK'&&city!=0) {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+          document.getElementById('message2').style.color = 'green';
+          document.getElementById('message2').innerHTML = 'Found The location';
+          setTimeout(function(){
+            document.getElementById("message2").innerHTML="";
+            },3000);
+      } else {
+        document.getElementById('message2').style.color = 'red';
+        document.getElementById('message2').innerHTML = 'Didnt Find the location';
+        setTimeout(function(){
+          document.getElementById("message2").innerHTML="";
+          },3000);
+      }
+    });
+  }
