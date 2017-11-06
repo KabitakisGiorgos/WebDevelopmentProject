43124ab21e1f4d7a9745a3e58a5aa262d02dc6e4
Giorgos Kampitakis <csd3142@csd.uoc.gr>
2017-11-07 00:15:04 +0200

    elegxos ean i callback eiani sinartisi kai orizoyme to this

--- a2/ourMar.js
+++ a2/ourMar.js
@@ -4,5 +4,6 @@ function ourMap(array){
     return (function mapWithCallback(fn,callback){
         if(typeof fn!=='function') return currentArr;
         currentArr=currentArr.map(fn);
+        if(typeof callback==='function') callback.call(currentArr,array);
     })
 }
