7f2a379bfd38fd7529ba61afeeef700af50d7aac
Giorgos Kampitakis <csd3142@csd.uoc.gr>
2017-11-07 00:13:33 +0200

    o elegxos ean to fn den eiani function kanoyme return ton currArray

--- a2/ourMar.js
+++ a2/ourMar.js
@@ -2,6 +2,6 @@ function ourMap(array){
     var originalArr=array;
     var currentArr=array;
     return (function mapWithCallback(fn,callback){
-
+        if(typeof fn!=='function') return currentArr;
     })
 }
