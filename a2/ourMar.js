1a512b9e9fdb1f6af9702c66bee36ccf4cc4e779
Giorgos Kampitakis <csd3142@csd.uoc.gr>
2017-11-07 00:14:22 +0200

    kanoyme to map tou current array me tin sinartisi fn

--- a2/ourMar.js
+++ a2/ourMar.js
@@ -3,5 +3,6 @@ function ourMap(array){
     var currentArr=array;
     return (function mapWithCallback(fn,callback){
         if(typeof fn!=='function') return currentArr;
+        currentArr=currentArr.map(fn);
     })
 }
