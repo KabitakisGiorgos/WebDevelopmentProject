5ed377a5a6faa170382e32c0a1fce71a0674e0fe
Giorgos Kampitakis <csd3142@csd.uoc.gr>
2017-11-07 00:32:46 +0200

    some minor fixes to .js code

--- a2/lq/face.js
+++ a2/lq/face.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 var URL;
 var face_token;
 /*
@@ -14,33 +14,33 @@ var faceRec = (function () {
 
   // Object that holds anything related with the facetPlusPlus REST API Service
   var faceAPI = {
-    apiKey: 'l2jNgKbk1HXSR4vMzNygHXx2g8c_xT9c',
-    apiSecret: '2T6XdZt4EYw-I7OhmZ6g1wtECl81e_Ip',
-    app: 'hy359',
+    apiKey: "l2jNgKbk1HXSR4vMzNygHXx2g8c_xT9c",
+    apiSecret: "2T6XdZt4EYw-I7OhmZ6g1wtECl81e_Ip",
+    app: "hy359",
     // Detect
     // https://console.faceplusplus.com/documents/5679127
-    detect: 'https://api-us.faceplusplus.com/facepp/v3/detect',  // POST
+    detect: "https://api-us.faceplusplus.com/facepp/v3/detect",  // POST
     // Set User ID
     // https://console.faceplusplus.com/documents/6329500
-    setuserId: 'https://api-us.faceplusplus.com/facepp/v3/face/setuserid', // POST
+    setuserId: "https://api-us.faceplusplus.com/facepp/v3/face/setuserid", // POST
     // Get User ID
     // https://console.faceplusplus.com/documents/6329496
-    getDetail: 'https://api-us.faceplusplus.com/facepp/v3/face/getdetail', // POST
+    getDetail: "https://api-us.faceplusplus.com/facepp/v3/face/getdetail", // POST
     // addFace
     // https://console.faceplusplus.com/documents/6329371
-    addFace: 'https://api-us.faceplusplus.com/facepp/v3/faceset/addface', // POST
+    addFace: "https://api-us.faceplusplus.com/facepp/v3/faceset/addface", // POST
     // Search
     // https://console.faceplusplus.com/documents/5679127
-    search: 'https://api-us.faceplusplus.com/facepp/v3/search', // POST
+    search: "https://api-us.faceplusplus.com/facepp/v3/search", // POST
     // Create set of faces
     // https://console.faceplusplus.com/documents/6329329
-    create: 'https://api-us.faceplusplus.com/facepp/v3/faceset/create', // POST
+    create: "https://api-us.faceplusplus.com/facepp/v3/faceset/create", // POST
     // update
     // https://console.faceplusplus.com/documents/6329383
-    update: 'https://api-us.faceplusplus.com/facepp/v3/faceset/update', // POST
+    update: "https://api-us.faceplusplus.com/facepp/v3/faceset/update", // POST
     // removeface
     // https://console.faceplusplus.com/documents/6329376
-    removeFace: 'https://api-us.faceplusplus.com/facepp/v3/faceset/removeface', // POST
+    removeFace: "https://api-us.faceplusplus.com/facepp/v3/faceset/removeface", // POST
   };
 
   // Object that holds anything related with the state of our append
@@ -55,7 +55,7 @@ var faceRec = (function () {
     // function that converts the dataURL to a binary blob object
     function dataURLtoBlob(dataUrl) {
       // Decode the dataURL
-      var binary = atob(dataUrl.split(',')[1]);
+      var binary = atob(dataUrl.split(",")[1]);
 
       // Create 8-bit unsigned array
       var array = [];
@@ -65,62 +65,59 @@ var faceRec = (function () {
 
       // Return our Blob object
       return new Blob([new Uint8Array(array)], {
-        type: 'image/jpg',
+        type: "image/jpg",
       });
     }
 
-    var fullQuality = canvas.toDataURL('image/jpeg', 1.0);
+    var fullQuality = canvas.toDataURL("image/jpeg", 1.0);
     return dataURLtoBlob(fullQuality);
 
   }
 
   // function that returns a base64 representation of the canvas
   function getImageAsBase64FromCanvas(canvas) {
-    return canvas.toDataURL('image/jpeg', 1.0);
+    return canvas.toDataURL("image/jpeg", 1.0);
 
   }
 
   // Function called when we upload an image
   function uploadImage() {
       var data = new FormData();
-      data.append('api_key', faceAPI.apiKey);
-      data.append('api_secret', faceAPI.apiSecret);
-      data.append('image_url',URL);
-  
-      ajaxRequest('POST', faceAPI.detect, data,function (my_response){
+      data.append("api_key", faceAPI.apiKey);
+      data.append("api_secret", faceAPI.apiSecret);
+      data.append("image_url",URL);
+      ajaxRequest("POST", faceAPI.detect, data,function (my_response){
         var data2=new FormData();
-        data2.append('api_key', faceAPI.apiKey);
-        data2.append('api_secret', faceAPI.apiSecret);
-        data2.append('face_token',my_response);
-        data2.append('user_id',document.getElementById("username").value);
+        data2.append("api_key", faceAPI.apiKey);
+        data2.append("api_secret", faceAPI.apiSecret);
+        data2.append("face_token",my_response);
+        data2.append("user_id",document.getElementById("username").value);
 
-        ajaxRequest2('POST',faceAPI.setuserId,data2,function(){
+        ajaxRequest2("POST",faceAPI.setuserId,data2,function(){
           var data3=new FormData();
-          data3.append('api_key', faceAPI.apiKey);
-          data3.append('api_secret', faceAPI.apiSecret);
-          data3.append('outer_id',faceAPI.app);
-          data3.append('face_tokens',my_response);
-          ajaxRequest3('POST',faceAPI.addFace,data3);
+          data3.append("api_key", faceAPI.apiKey);
+          data3.append("api_secret", faceAPI.apiSecret);
+          data3.append("outer_id",faceAPI.app);
+          data3.append("face_tokens",my_response);
+          ajaxRequest3("POST",faceAPI.addFace,data3);
         });
       });
   }
 
   function uploadImage2(URL2){
     var data=new FormData();
-    data.append('api_key', faceAPI.apiKey);
-    data.append('api_secret', faceAPI.apiSecret);
-    data.append('image_url',URL2);
-    data.append('outer_id',faceAPI.app);
-    ajaxRequest4('POST', faceAPI.search, data);
+    data.append("api_key", faceAPI.apiKey);
+    data.append("api_secret", faceAPI.apiSecret);
+    data.append("image_url",URL2);
+    data.append("outer_id",faceAPI.app);
+    ajaxRequest4("POST", faceAPI.search, data);
   }
   // Function for initializing things (event handlers, etc...)
   function init() {
       // Trigger when upload button is pressed
-      document.getElementById('upload').addEventListener('check', uploadImage);
+      document.getElementById("upload").addEventListener("check", uploadImage);
   }
-
  
-
   function ajaxRequest(post,search,data,callback){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -171,15 +168,15 @@ var faceRec = (function () {
         if(myresponse.results.length>=1){
             if(myresponse.results[0].confidence>=78){
                 document.getElementById("username").value=myresponse.results[0].user_id;
-                document.getElementById("nomatch").innerHTML='';
+                document.getElementById("nomatch").innerHTML="";
             }else{
-              document.getElementById("nomatch").innerHTML='You are too beautiful to match you with somenone';
+              document.getElementById("nomatch").innerHTML="You are too beautiful to match you with somenone";
               setTimeout(function(){
                 document.getElementById("nomatch").innerHTML="";
                 },3000);
             }
         }else{
-          document.getElementById("nomatch").innerHTML='You are too beautiful to match you with somenone';
+          document.getElementById("nomatch").innerHTML="You are too beautiful to match you with somenone";
           setTimeout(function(){
             document.getElementById("nomatch").innerHTML="";
             },3000);
@@ -201,14 +198,14 @@ var faceRec = (function () {
 
 function URLfunction(){
   URL = prompt("Please enter an image URL", "https://goo.gl/AES9iA");
-  document.getElementById("upload").style.display='inline';
+  document.getElementById("upload").style.display="inline";
 }
 
 function URLfunction2(value){
-  if(value==='yes'){
+  if(value==="yes"){
       var URL2 = prompt("Please enter an image URL", "https://goo.gl/AES9iA");
       if(URL2==null) {
-          document.getElementById('no').checked=true;
+          document.getElementById("no").checked=true;
       }else{
         document.getElementById("nomatch").innerHTML="Wait you handsome beast";
         faceRec.uploadImage2(URL2);
--- a2/lq/myCode.js
+++ a2/lq/myCode.js
@@ -1,24 +1,24 @@
-"use strict"
+"use strict";
 var passwordcheckin=false;
 
 var verify = function(e,flag) {
-    if ((document.getElementById('password').value ==
-      document.getElementById('confirm_password').value&&e.keyCode===13)||(document.getElementById('password').value ==
-      document.getElementById('confirm_password').value&&flag===0)) {
+    if ((document.getElementById("password").value ==
+      document.getElementById("confirm_password").value&&e.keyCode===13)||(document.getElementById("password").value ==
+      document.getElementById("confirm_password").value&&flag===0)) {
         setTimeout(function(){
           document.getElementById("message2").innerHTML="";
           },3000);
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'Correct';
+      document.getElementById("message").style.color = "green";
+      document.getElementById("message").innerHTML = "Correct";
       passwordcheckin=true;
     } else if(e.keyCode===13||flag===0){
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'Wrong Password';
+      document.getElementById("message").style.color = "red";
+      document.getElementById("message").innerHTML = "Wrong Password";
       setTimeout(function(){
         document.getElementById("message").innerHTML="";
         },3000);
     }
-  }
+  };
 
   var askPass=function(){
 
@@ -26,30 +26,30 @@ var verify = function(e,flag) {
      alert("Two passwords are not the same");
      event.preventDefault();
    }
-  }
+  };
 
   function initMap() {
-    document.getElementById('city').addEventListener('blur', function() {
+    document.getElementById("city").addEventListener("blur", function() {
       
-      var map = new google.maps.Map(document.getElementById('map'), {
+      var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: {lat: -34.397, lng: 150.644}
       });
       var geocoder = new google.maps.Geocoder();
       geocodeAddress(geocoder, map);
     });
-    document.getElementById('address').addEventListener('blur', function() {
+    document.getElementById("address").addEventListener("blur", function() {
       
-      var map = new google.maps.Map(document.getElementById('map'), {
+      var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: {lat: -34.397, lng: 150.644}
       });
       var geocoder = new google.maps.Geocoder();
       geocodeAddress(geocoder, map);
     });
-    document.getElementById('country').addEventListener('blur', function() {
+    document.getElementById("country").addEventListener("blur", function() {
       
-      var map = new google.maps.Map(document.getElementById('map'), {
+      var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: {lat: -34.397, lng: 150.644}
       });
@@ -60,53 +60,53 @@ var verify = function(e,flag) {
 
   function geocodeAddress(geocoder, resultsMap) {
 
-    var address = document.getElementById('address').value;
-    var city = document.getElementById('city').value;
-    var country = document.getElementById('country').value;
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("city").value;
+    var country = document.getElementById("country").value;
     var address2=country+city+address;
-    geocoder.geocode({'address': address2}, function(results, status) {
+    geocoder.geocode({"address": address2}, function(results, status) {
       
-      if (status === 'OK'&&city!=0) {
+      if (status === "OK"&&city!=0) {
         resultsMap.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
           map: resultsMap,
           position: results[0].geometry.location
         });
-            document.getElementById('showbutton').style.display="block";
+            document.getElementById("showbutton").style.display="block";
             console.log("found location");
             
       } else {
-        document.getElementById('message2').style.color = 'red';
-        document.getElementById('message2').innerHTML = 'Didnt Find the location';
+        document.getElementById("message2").style.color = "red";
+        document.getElementById("message2").innerHTML = "Didnt Find the location";
         setTimeout(function(){
           document.getElementById("message2").innerHTML="";
           },3000);
-          document.getElementById('showbutton').style.display="none";
-          document.getElementById('hidebutton').style.display="none";
-          document.getElementById('map').style.display = 'none'
+          document.getElementById("showbutton").style.display="none";
+          document.getElementById("hidebutton").style.display="none";
+          document.getElementById("map").style.display = "none";
       }
     });
   }
 
 var Showmap=function(){
-  document.getElementById('map').style.display = 'block';
-  document.getElementById('map').style.width = '100%';
-  document.getElementById('map').style.height = '400px';
-   document.getElementById('hidebutton').style.display="inline";
+  document.getElementById("map").style.display = "block";
+  document.getElementById("map").style.width = "100%";
+  document.getElementById("map").style.height = "400px";
+   document.getElementById("hidebutton").style.display="inline";
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: {lat: -34.397, lng: 150.644}
   });
   var geocoder = new google.maps.Geocoder();
   geocodeAddress(geocoder, map);
-}
+};
 
 var Hidemap=function(){
-  document.getElementById('map').style.display = 'none';
-  document.getElementById('hidebutton').style.display = 'none';
-}
+  document.getElementById("map").style.display = "none";
+  document.getElementById("hidebutton").style.display = "none";
+};
 
 function buttonappear(){
-  if(document.getElementById("username").value.match(/[A-Za-z0-9]{8,}/)) document.getElementById("snap").style.display='inline';
+  if(document.getElementById("username").value.match(/[A-Za-z0-9]{8,}/)) document.getElementById("snap").style.display="inline";
 }
